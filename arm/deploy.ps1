28863db4465e520d6cee8eddc17ff12c0a178d27
Christian Weyer <christian.weyer@thinktecture.com>
2018-10-09 21:08:24 +0200

    Updated ARM templates

--- arm/deploy.ps1
+++ arm/deploy.ps1
@@ -75,7 +75,7 @@ Write-Host "Selecting subscription '$subscriptionId'";
 Select-AzureRmSubscription -SubscriptionID $subscriptionId;
 
 # Register RPs
-$resourceProviders = @("microsoft.documentdb","microsoft.insights","microsoft.servicebus","microsoft.storage","microsoft.web");
+$resourceProviders = @("microsoft.documentdb","microsoft.insights","microsoft.search","microsoft.servicebus","microsoft.storage","microsoft.web");
 if($resourceProviders.length) {
     Write-Host "Registering resource providers"
     foreach($resourceProvider in $resourceProviders) {
